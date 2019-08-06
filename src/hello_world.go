3c14142f7f4cf29f9e93209b155b6ab7b5bbd3c1
phoenixviny <alone.sugar@gmail.com>
2019-08-06 11:38:19 +0800

    add floder

---
+++ src/hello_world.go
@@ -0,0 +1,11 @@
+// Every program should have a main package
+package main
+
+// fmt package implements formatted I/O with functions analogous to C's printf and scanf. Pronounce is "fumpt"
+import "fmt"
+
+// Every program should have a main function too, and will be the first executed function.
+func main() {
+	// Println prints "Hello World!!" on the standard output and also break a line.
+	fmt.Println("Hello World!!")
+}
